8e317dfbc9c9df605da69c1a94b880b15952cf37
Your Name <you@example.com>
2025-06-07 11:01:46 +0530

    Updated

--- p2/script.js
+++ p2/script.js
@@ -1,15 +1,17 @@
+const users = []
 const showLogin = () => {
     let str=`
     <div>
     <h1>Login Form</h1>
+    <p><div id="dvMsg"></div></p>
     <p><input type="text" id="txtEmail"></p>
     <p><input type="password" id="txtPass"></p>
-    <p><button onclick='showHome()'>Log In</button></p>
+    <p><button onclick='validateUser()'>Log In</button></p>
     <p><button onclick='showRegister()'>Create Account</button></p>
     </div>
     `
     root.innerHTML = str
-}
+};
 
 const showRegister = () => {
       let str=`
@@ -17,16 +19,54 @@ const showRegister = () => {
     <p><input type="text" id="txtName"></p>
      <p><input type="text" id="txtEmail"></p>
     <p><input type="password" id="txtPass"></p>
-    <button>Register</button>
+    <button onClick='addUser()'>Register</button>
     <hr>
-    <button onClick='showLogin()'>Alread a Member? Login here...</button>
+    <button onClick='showLogin()'>Already a Member? Login here...</button>
     `
     root.innerHTML = str
-}
+};
 
-const showHome = () => {
+const showHome = (user) => {
     let str = `
-    <h1>Welcome</h1>
-    `
+    <h1>Welcome ${user.name}</h1>
+    <hr>
+    <select>
+        <option value=0>--select--</option>
+        <option value=1>Deposit</option>
+        <option value=2>Withdraw</option>
+    </select>
+    <p>
+    <input type='number' id='txtAmount'>
+    </p>
+    <button>Submit</button>
+    <button onClick='showLogin()'>Logout</button>
+    </p>
+    <p>Current Balance:${user.balance}
+    `;
     root.innerHTML=str
-}
+};
+
+const addUser = () => {
+    const user = {
+        name: document.getElementById("txtName").value,
+        email: document.getElementById("txtEmail").value,
+        pass:document.getElementById("txtPass").value,
+        balance:0
+    }
+    users.push(user)
+    console.log(users)
+    showLogin()
+};
+
+const validateUser = () => {
+    let email= document.getElementById("txtEmail").value;
+    let pass= document.getElementById("txtPass").value;
+    const found = users.find(user=> user.email === email && user.pass === pass)
+    console.log(found);
+    if (found) {
+    showHome(found);
+    } else {
+        document.getElementById("dvMsg").innerHTML = "Access Denied!";
+    }
+
+};
