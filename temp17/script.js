6759dda0486b16726569145a4bef3c1f87ace8ef
Your Name <you@example.com>
2025-06-09 11:08:47 +0530

    updated

--- temp17/script.js
+++ temp17/script.js
@@ -1,12 +1,20 @@
-//async
+//asynchronous - concept of Promise.reject and Promise.resolve
+// fetch returns a promise!-- here we used f1() in place of fetch(), look at temp10 and this code side by side
+// most common/standard example of the promise thingy is online banking site.
 
-function f1(){
+function f1(x){
+    if(x<0){
+        return Promise.reject("something went wrong!");
+    }
+    else{
+        return Promise.resolve(x);
+    }
     /*
     setTimeout(() => {
     return 5;
     }, 1000);
     */
-    return Promise.resolve(5);
+    //return Promise.resolve(5); // we return resolve on if everything went well. else, if somethign went wrong, we send the promise.reject . 
     //return Promise.reject("error");
 }
 
@@ -14,8 +22,8 @@ function f2(a){
     console.log(a);
 }
 
-f1()
-    .then((v) => f2(v))
+f1(-10)
+    .then((v) => {f2(v)})
     .catch((err)=>console.log(err));
 
 
